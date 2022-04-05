f6176129af50cb08a8f91afed55a15f70668b27c
João <joaoviniciusdacosta800@gmail.com>
2022-04-05 17:54:11 -0300

    ajustes

--- includes/header.php
+++ includes/header.php
@@ -6,31 +6,19 @@
 				<li style="white-space: nowrap;">
                     <a href="index.php">Home</a>
                 </li>
-                    <li class="opener" style="user-select: none; cursor: pointer; white-space: nowrap;">
-                        <a href="#" class="icon solid fa-angle-down">Layouts</a>	
-                        <ul class="" style="user-select: none; display: none; position: absolute;">
-                            <li style="white-space: nowrap;">
-                                <a href="view/generic.php" style="display: block;">Generic</a>
-                            </li>
-                            <li style="white-space: nowrap;">
-                                <a href="view/contact.php" style="display: block;">Contact</a>
-                            </li>
-                            <li style="white-space: nowrap;">
-                                <a href="view/elements.php" style="display: block;">Elements</a>
-                            </li>
-                            <li class="opener" style="user-select: none; cursor: pointer; white-space: nowrap;">
-                                <a href="#" style="display: block;">Submenu</a>
-                                <ul class="dropotron" style="user-select: none; display: none; position: absolute;">
-                                    <li style="white-space: nowrap;"><a href="#" style="display: block;">Option One</a></li>
-                                    <li style="white-space: nowrap;"><a href="#" style="display: block;">Option Two</a></li>
-                                    <li style="white-space: nowrap;"><a href="#" style="display: block;">Option Three</a></li>
-                                    <li style="white-space: nowrap;"><a href="#" style="display: block;">Option Four</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                <li style="white-space: nowrap;">
-                    <a href="#" class="button">Sign Up</a>
+                <li class="opener" style="user-select: none; cursor: pointer; white-space: nowrap;">
+                    <a href="#" class="icon solid fa-angle-down">Layouts</a>	
+                    <ul class="" style="user-select: none; display: none; position: absolute;">
+                        <li style="white-space: nowrap;">
+                            <a href="view/generic.php" style="display: block;">Generic</a>
+                        </li>
+                        <li style="white-space: nowrap;">
+                            <a href="view/contact.php" style="display: block;">Contact</a>
+                        </li>
+                        <li style="white-space: nowrap;">
+                            <a href="view/elements.php" style="display: block;">Elements</a>
+                        </li>
+                    </ul>
                 </li>
 			</ul>
 		</nav>
