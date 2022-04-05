eb5254f77f70ded068f08ac8f04b96fc2f2fd028
Gabriel Teixeira <gabriel.404e@gmail.com>
2022-04-05 17:10:59 -0300

    organizar git

--- includes/css_imports.php
+++
@@ -1,1 +0,0 @@
-<link rel="stylesheet" href="../public/assets/css/main.css" />
--- includes/scripts_imports.php
+++
@@ -1,7 +0,0 @@
-<script src="../public/assets/js/lib/jquery.min.js"></script>
-<script src="../public/assets/js/lib/jquery.dropotron.min.js"></script>
-<script src="../public/assets/js/lib/jquery.scrollex.min.js"></script>
-<script src="../public/assets/js/lib/browser.min.js"></script>
-<script src="../public/assets/js/lib/breakpoints.min.js"></script>
-<script src="../public/assets/js/lib/util.js"></script>
-<script src="../public/assets/js/lib/main.js"></script>
