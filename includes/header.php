cf06939fb01b2335247b97a5bd3583e31428b764
João <joaoviniciusdacosta800@gmail.com>
2022-04-05 18:34:26 -0300

    save

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <header id="header" class="alt">
 	<h1>
-        <a href="index.php">CONSTRU</a>C<span style="color:#f00">A</span>MPI</h1>
+        <a href="../index.php">CONSTRU</a>C<span style="color:#f00">A</span>MPI</h1>
 		<nav id="nav">
 			<ul>
 				<li style="white-space: nowrap;">
@@ -15,27 +15,13 @@
                         <li style="white-space: nowrap;">
                             <a href="view/contact.php" style="display: block;">Obras públicas</a>
                         </li>
-                        <li style="white-space: nowrap;">
-                            <a href="view/elements.php" style="display: block;">Elements</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="opener" style="user-select: none; cursor: pointer; white-space: nowrap;">
-                    <a href="#" class="icon solid fa-angle-down">Galpões e estruturas metálicas em geral</a>	
-                    <ul class="" style="user-select: none; display: none; position: absolute;">
-                        <li style="white-space: nowrap;">
-                            <a href="view/generic.php" style="display: block;">Generic</a>
-                        </li>
-                        <li style="white-space: nowrap;">
-                            <a href="view/contact.php" style="display: block;">Contact</a>
-                        </li>
-                        <li style="white-space: nowrap;">
-                            <a href="view/elements.php" style="display: block;">Elements</a>
-                        </li>
                     </ul>
                 </li>
                 <li style="white-space: nowrap;">
-                    <a href="index.php">Minha casa verde e amarela</a>
+                    <a href="../index.php">Galpões e estruturas metálicas em geral</a>	
+                </li>
+                <li style="white-space: nowrap;">
+                    <a href="../index.php">Minha casa verde e amarela</a>
                 </li>
 			</ul>
 		</nav>
--- index.php
+++ index.php
@@ -91,23 +91,10 @@
 
 				</section>
 
-			<!-- CTA -->
-				<section id="cta">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3659.103124475027!2d-46.85204328502365!3d-23.492794784716946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf02394b674fc9%3A0x66136606865c6ceb!2sEdificio%20Amazonia!5e0!3m2!1spt-BR!2sbr!4v1649184281592!5m2!1spt-BR!2sbr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-					<!-- <h2>Sign up for beta access</h2>
-					<p>Blandit varius ut praesent nascetur eu penatibus nisi risus faucibus nunc.</p>
+				<section>
 
-					<form>
-						<div class="row gtr-50 gtr-uniform">
-							<div class="col-8 col-12-mobilep">
-								<input type="email" name="email" id="email" placeholder="Email Address" />
-							</div>
-							<div class="col-4 col-12-mobilep">
-								<input type="submit" value="Sign Up" class="fit" />
-							</div>
-						</div>
-					</form> -->
-
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3659.103124475027!2d-46.85204328502365!3d-23.492794784716946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf02394b674fc9%3A0x66136606865c6ceb!2sEdificio%20Amazonia!5e0!3m2!1spt-BR!2sbr!4v1649184281592!5m2!1spt-BR!2sbr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				
 				</section>
 
 			<!-- Footer -->
