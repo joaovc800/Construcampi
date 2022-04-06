200d0d3c3df915529f73257d428403bf3d36a2fc
gabriel-b <gabriel.404e@gmail.com>
2022-04-06 20:54:17 -0300

    remover env

--- bundle_dev/api/controllers/error.js
+++
@@ -1,5 +0,0 @@
-export function page404(req, res, next) {
-    res.status(404).render('page404', {
-        pageTitle : 'Page not found', path : '404'
-        });
-}
--- bundle_dev/nginx/client/dev/iniciar-servicos-dev.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-php-fpm
-
-# If on windows, change CRLF to LF else bash will not work
