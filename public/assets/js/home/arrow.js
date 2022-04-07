c13432fd0398396f69cf7936d8983366b485eee1
Joao Vinicius <joaoviniciusdacosta800@gmail.com>
2022-04-07 16:23:51 -0300

    save changes

--- public/assets/js/home/arrow.js
+++ public/assets/js/home/arrow.js
@@ -30,7 +30,7 @@ $(function(){
 
     $(window).scroll(function(){
         var scrollBottom =  $(window).height() - $(window).scrollTop();
-        if(scrollBottom <= 20){
+        if(scrollBottom <= 40){
             $(".animate-push-left").addClass("push-left")
             $(".animate-push-left").removeClass("animate-push-left")
         }
