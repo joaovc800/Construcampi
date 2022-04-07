eed76a2578d3458c572fbef1bd8b35cbf48f00ea
Gabriel Teixeira <gabriel.404e@gmail.com>
2022-04-07 14:21:16 -0300

    adicionar resposividade ao video

--- public/assets/js/home/home.js
+++ public/assets/js/home/home.js
@@ -2,7 +2,6 @@
 
 
 $(function(){
-
   window.exit_modal = function exit_modal() { // sair do modal
     $("div_modal_video").modal("hide");
     var nodeModal = document.getElementById("div_modal_video");
@@ -24,7 +23,7 @@ $(function(){
     var div = criaElemento({ element: "div", id: "div_modal_video"});
     div.classList.add("modal");
     document.body.prepend(div);
-
+    
     const modal = `
       <div class="modal-dialog modal-fullscreen">
         <div class="modal-content" style="background-color:#222222; z-index:"20000;">
@@ -33,10 +32,9 @@ $(function(){
             <button type="button" class="btn-close btn-close-white" onclick="exit_modal()" data-bs-dismiss="modal"></button>
           </div>
           <div class="modal-body" id="modal-body" style="position:relative;">
-          <video preload="auto" autoplay loop style="position:absolute;left:0;right:0;width:100%;">
-            <source src="./public/videos/video_site_completo.mp4">
-          </video>
+            
           </div>
+          
           <div class="modal-footer" style="border-top:1px solid #ae9625; justify-content:space-between;">
             <h5 style="color:#eeeeee;">&copy; Construcampi 2022. All rights reserved.</h5>
             <button type="button" class="btn btn-secondary" onclick="exit_modal()" data-bs-dismiss="modal">Close</button>
@@ -45,7 +43,31 @@ $(function(){
     </div>
     `
     document.getElementById("div_modal_video").innerHTML = modal;
+    // Depois criar outra funcao dentro da query para apenas calcular e uma para setar, esta de calc sendo inserida na de setar
+    // Basicamente estamos fazendo o calculo do CSS aqui (100%) para criar um zoom.
+    function getAbsoluteHeight() {
+      const element = document.getElementById('modal-body');
+      const elementPlayer = document.getElementById('frame1');
+
+      let elHeight = $(element).height();
+      let elWidth = $(element).width();
+
+      elementPlayer.style.width = `${elWidth + 30}px`; // + 30 é a margin
+      elementPlayer.style.height = `${elHeight}px`;
+    }
+
     $("#div_modal_video").modal("show");  
+    $(window).resize(getAbsoluteHeight);
+
+    // isto é para ser limpado depois com as novas funcoes acimas
+    // Adicionar para rodar a primeira vez o width
+    const element = document.getElementById('modal-body');
+
+    let elHeight = $(element).height();
+    let elWidth = $(element).width();
+
+    element.innerHTML = `<iframe id="frame1" src="https://www.youtube.com/embed/LHw48W4Fcy4?rel=0?Version=3&showinfo=0&autoplay=1&loop=1" style="position:absolute;left:0;right:0;height:${elHeight}px;width:${elWidth + 30}px;"  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+    
   }
 
   // Criar elemento do modal
