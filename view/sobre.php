731d6186c547203fef348ab1b926590b9e95ada7
Joao Vinicius <joaoviniciusdacosta800@gmail.com>
2022-04-11 14:43:26 -0300

    save

--- view/sobre.php
+++ view/sobre.php
@@ -74,52 +74,52 @@
 							<header class="major">
 								<h2>Ipsum sed dolor</h2>
 							</header>
-							<div class="posts animate-push-left">
+							<div class="posts">
 								<article>
 									<a href="#" class="image"><img src="images/pic01.jpg" alt="" /></a>
-									<h3>Interdum aenean</h3>
-									<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-									<ul class="actions">
+									<h3 class="animate-push-left">Interdum aenean</h3>
+									<p class="animate-push-left">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+									<ul class="actions animate-push-left">
 										<li><a href="#" class="button">More</a></li>
 									</ul>
 								</article>
 								<article>
 									<a href="#" class="image"><img src="images/pic02.jpg" alt="" /></a>
-									<h3>Nulla amet dolore</h3>
-									<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-									<ul class="actions">
+									<h3 class="animate-push-left">Nulla amet dolore</h3>
+									<p class="animate-push-left">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+									<ul class="actions animate-push-left">
 										<li><a href="#" class="button">More</a></li>
 									</ul>
 								</article>
 								<article>
 									<a href="#" class="image"><img src="images/pic03.jpg" alt="" /></a>
-									<h3>Tempus ullamcorper</h3>
-									<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-									<ul class="actions">
+									<h3 class="animate-push-left">Tempus ullamcorper</h3>
+									<p class="animate-push-left">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+									<ul class="actions animate-push-left">
 										<li><a href="#" class="button">More</a></li>
 									</ul>
 								</article>
 								<article>
 									<a href="#" class="image"><img src="images/pic04.jpg" alt="" /></a>
-									<h3>Sed etiam facilis</h3>
-									<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+									<h3 class="animate-push-left">Sed etiam facilis</h3>
+									<p class="animate-push-left">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
 									<ul class="actions">
 										<li><a href="#" class="button">More</a></li>
 									</ul>
 								</article>
 								<article>
 									<a href="#" class="image"><img src="images/pic05.jpg" alt="" /></a>
-									<h3>Feugiat lorem aenean</h3>
-									<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-									<ul class="actions">
+									<h3 class="animate-push-left">Feugiat lorem aenean</h3>
+									<p class="animate-push-left">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+									<ul class="actions animate-push-left">
 										<li><a href="#" class="button">More</a></li>
 									</ul>
 								</article>
 								<article>
 									<a href="#" class="image"><img src="images/pic06.jpg" alt="" /></a>
-									<h3>Amet varius aliquam</h3>
-									<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-									<ul class="actions">
+									<h3 class="animate-push-left">Amet varius aliquam</h3>
+									<p class="animate-push-left">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+									<ul class="actions animate-push-left">
 										<li><a href="#" class="button">More</a></li>
 									</ul>
 								</article>
