eb9b428978710886a3f3dbdb509f6c25156a8b6a
Joao Vinicius <joaoviniciusdacosta800@gmail.com>
2022-04-12 11:46:11 -0300

    save open graph and SEO

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 		<meta property="og:description" content="Venha conhecer a Construcampi, seu objetivo é o nosso principal compromisso e responsabilidade.">
 		<meta name="description" content="Venha conhecer a Construcampi, seu objetivo é o nosso principal compromisso e responsabilidade.">
 		<meta property="og:image" content="public/images/cc1.jpg">
+		<meta property="og:image:type" content="image/jpg">
 		<!-- <meta property="og:image" itemprop="image" content="https://barbeariatikos.herokuapp.com/imgs/logo.jpeg"> -->
 		<meta property="og:image:width" content="300">
 		<meta property="og:image:height" content="300">
