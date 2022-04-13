c3588082250f6d08cb5f57fe3a23c1bbad8017b2
Joao Vinicius <joaoviniciusdacosta800@gmail.com>
2022-04-13 14:32:44 -0300

    save

--- view/sobre.php
+++ view/sobre.php
@@ -26,8 +26,8 @@
 							<h1 class="h2">Olá, somos a Construcampi</h1>
 							<p>Conheça mais sobre nossa tragetória...</p>
 						</header>
-						<p>Pioneira no ramo da engenharia civil, a Construcampi possui mais de 30 anos de mercado, sempre trazendo soluções e serviços inovadores, garantindo sempre a satisfação e fidelização de seus clientes.<br>
-							De construções de alto padrão a serviços de infraestrutura, estamos sempre preparados para fornecer o melhor atendimento e os melhores resultados, sempre implementando a dedicação, esforço e experiência adquiridas durante nossa trajetória.</p>
+						<p>Pioneira no ramo da engenharia civil, a Construcampi possui mais de 30 anos de mercado, trazendo soluções e serviços inovadores, garantindo a satisfação e fidelização de seus clientes.<br>
+							De construções de alto padrão a serviços de infraestrutura, estamos preparados para fornecer o melhor atendimento e os melhores resultados, sempre implementando a dedicação, esforço e experiência adquiridas durante nossa trajetória.</p>
 					</div>
 					<span class="image object">
 						<img src="../public/images/cc3.jpg" alt="" class="grow_image" />
@@ -41,14 +41,14 @@
 							<span class="icon fa-gem"></span>
 							<div class="content">
 								<h3>SOLUÇÕES MODERNAS E INOVADORAS</h3>
-								<p>Possuímos o mais alto desempenho em inovação e criatividade no mercado, sempre trazendo serviços de qualidade e de alta modernidade no ramo da construção civil. Contamos com os mais altos padrões tecnológicos disponíveis no mercado para a realização de nossas construções, sendo a rapidez e a eficiência lemas primordiais em nossa empresa.</p>
+								<p>Possuímos o mais alto desempenho em inovação e criatividade no mercado, trazendo serviços de qualidade e de alta modernidade no ramo da construção civil. Contamos com os mais altos padrões tecnológicos disponíveis no mercado para a realização de nossas construções, sendo a rapidez e a eficiência lemas primordiais em nossa empresa.</p>
 							</div>
 						</article>
 						<article>
 							<span class="icon solid fa-paper-plane"></span>
 							<div class="content">
 								<h3>ACESSIBILIDADE</h3>
-								<p>Trabalhamos sempre para que nosso trabalho possa atingir a maior quantidade de pessoas possível, por isso, implementamos, em nossos valores e meios de comunicação, soluções acessíveis para o contato empresa x cliente, estabelecendo um formato de relação mais inclusivo dentro do mercado.</p>
+								<p>Trabalhamos para que nosso trabalho possa atingir a maior quantidade de pessoas possível, por isso, implementamos, em nossos valores e meios de comunicação, soluções acessíveis para o contato empresa x cliente, estabelecendo um formato de relação mais inclusivo dentro do mercado.</p>
 							</div>
 						</article>
 						<article>
