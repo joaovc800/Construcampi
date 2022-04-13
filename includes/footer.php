6aca89fa8a46ae0e6f0612c677e471b866c14873
Joao Vinicius <joaoviniciusdacosta800@gmail.com>
2022-04-13 12:57:11 -0300

    save

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,86 @@
 <footer id="footer">
-    <ul class="icons" style="padding-left:0; margin: 0 0 2em 0;">
-        <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-        <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-        <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-        <li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
-        <li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
-        <li><a href="#" class="icon brands fa-google-plus"><span class="label">Google+</span></a></li>
-    </ul>
-    <ul class="copyright">
-        <li>&copy; Construcampi. All rights reserved.</li><li>CNPJ: 52.167.111/0001-90</li>
-    </ul>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+               
+                <div class="mb-1">
+                    <a class="navbar-brand" aria-label="Space">
+                        <h1 class="text-light">
+                            CONSTRUC<span style="color:#f00">A</span>MPI
+                        </h1>
+                    </a>
+                </div>
+               
+                <ul class="list-unstyled list-py-1 mb-0">
+                    <li><a class="link-sm link-light">Copyright &copy 2022</a></li>
+                    <li><a class="link-sm link-light">Todos os diretos reservados</a></li>
+                    <li>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="link-sm link-light icon brands fa-twitter"><span
+                                        class="label">Twitter</span></a></li>
+                            <li><a href="#" class="link-sm link-light icon brands fa-facebook-f"><span
+                                        class="label">Facebook</span></a></li>
+                            <li><a href="#" class="link-sm link-light icon brands fa-instagram"><span
+                                        class="label">Instagram</span></a></li>
+                            <li><a href="#" class="link-sm link-light icon brands fa-github"><span
+                                        class="label">Github</span></a></li>
+                            <li><a href="#" class="link-sm link-light icon brands fa-dribbble"><span
+                                        class="label">Dribbble</span></a></li>
+                            <li><a href="#" class="link-sm link-light icon brands fa-google-plus"><span
+                                        class="label">Google+</span></a></li>
+                        </ul>
+                    </li>
+                </ul>
+
+            </div>
+
+            <div class="col-sm mb-7 mb-sm-0">
+                <h5 class="text-white mb-3">Obras</h5>
+
+                <ul class="list-unstyled list-py-1 mb-0">
+                    <li><a class="link-sm link-light" href="#">Obras privadas</a></li>
+                    <li><a class="link-sm link-light" href="#">Obras públicas</a></li>
+                    <li><a class="link-sm link-light" href="#">Galpões e estruturas metálicas</a></li>
+                    <li><a class="link-sm link-light" href="#">Reformas em geral</a></li>
+                    <li><a class="link-sm link-light" href="#">Casas pré-fabricadas</a></li>
+                    <li><a class="link-sm link-light" href="#">Pavimentações asfálticas</a></li>
+                </ul>
+                
+            </div>
+
+            <div class="col-sm mb-7 mb-sm-0">
+                <h5 class="text-white mb-3">A Construcampi</h5>
+
+                <ul class="list-unstyled list-py-1 mb-0">
+                    <li><a class="link-sm link-light" href="#">Quem somos</a></li>
+                    <li><a class="link-sm link-light" href="#">Políticas de privacidade</a></li>
+                </ul>
+            
+            </div>
+
+            <div class="col-sm">
+                <h5 class="text-white mb-3">Parceiros</h5>
+
+               
+                <ul class="list-unstyled list-py-1 mb-0">
+                    <li><a class="link-sm link-light" href="#">Parceirias públicas e privadas</a></li>
+                </ul>
+               
+            </div>
+
+            <div class="col-sm">
+                <h5 class="text-white mb-3">Programas</h5>
+
+                <ul class="list-unstyled list-py-1 mb-5">
+                    <li><a class="link-sm link-light" href="#">Casa verde e amarela</a></li>
+
+                </ul>
+                
+            </div>
+            
+        </div>
+
+    </div>
 </footer>
 <div vw class="enabled">
     <div vw-access-button class="active"></div>
@@ -18,5 +89,5 @@
     </div>
 </div>
 <div id="back" class="text-center">
-	<i class="fas h5 mt-1 text-light fa-angle-double-up"></i>
+    <i class="fas h5 mt-1 text-light fa-angle-double-up"></i>
 </div>
