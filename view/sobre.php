debefde5c96904eff01b1be5587af932aba67d75
Joao Vinicius <joaoviniciusdacosta800@gmail.com>
2022-04-13 14:42:55 -0300

    save

--- view/sobre.php
+++ view/sobre.php
@@ -38,28 +38,28 @@
 				<section>
 					<div class="features">
 						<article>
-							<span class="icon fa-gem"></span>
+							<span class="icon solid fa-chart-line"></span>
 							<div class="content">
 								<h3>SOLUÇÕES MODERNAS E INOVADORAS</h3>
 								<p>Possuímos o mais alto desempenho em inovação e criatividade no mercado, trazendo serviços de qualidade e de alta modernidade no ramo da construção civil. Contamos com os mais altos padrões tecnológicos disponíveis no mercado para a realização de nossas construções, sendo a rapidez e a eficiência lemas primordiais em nossa empresa.</p>
 							</div>
 						</article>
 						<article>
-							<span class="icon solid fa-paper-plane"></span>
+							<span class="icon solid fa-check"></span>
 							<div class="content">
 								<h3>ACESSIBILIDADE</h3>
 								<p>Trabalhamos para que nosso trabalho possa atingir a maior quantidade de pessoas possível, por isso, implementamos, em nossos valores e meios de comunicação, soluções acessíveis para o contato empresa x cliente, estabelecendo um formato de relação mais inclusivo dentro do mercado.</p>
 							</div>
 						</article>
 						<article>
-							<span class="icon solid fa-rocket"></span>
+							<span class="icon solid fa-balance-scale"></span>
 							<div class="content">
 								<h3>EFICIÊNCIA</h3>
 								<p>A Construcampi possui como objetivo trazer sempre trabalhos eficientes e de altíssima qualidade para nossos clientes, por isso, contamos com uma equipe sempre disponível e disposta a auxiliar nossos clientes das mais diversas maneiras, seja com consultorias e/ou atendimentos personalizados, nos adequando sempre para corresponder da melhor forma as expectativas de nossos clientes.</p>
 							</div>
 						</article>
 						<article>
-							<span class="icon solid fa-signal"></span>
+							<span class="icon solid fa-handshake"></span>
 							<div class="content">
 								<h3>QUALIDADE</h3>
 								<p>Na Construcampi, temos como prioridade a entrega de serviços com a maior qualidade dentro do ramo da engenharia civil, sempre com o intuito de trazer satisfação e confiabilidade para atender as suas expectativas.</p>
