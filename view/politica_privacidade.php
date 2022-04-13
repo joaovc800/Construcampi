5ca04efda3093599d54f308ea58a1926b7305e6e
Gabriel Teixeira <gabriel.404e@gmail.com>
2022-04-13 16:36:01 -0300

    Fix politicas header

--- view/politica_privacidade.php
+++ view/politica_privacidade.php
@@ -17,14 +17,17 @@
 			<!-- Main -->
 				<section id="main" class="container">
 					<header class="pt-5">
-						<h2>Política de Privacidade Construcampi</h2>
-						<p></p>
+						<h2>POLÍTICA DE PRIVACIDADE CONSTRUCAMPI</h2>
+						<p>A sua privacidade é importante para nós. </p>
 					</header>
 					<div class="box">
-						<p>A sua privacidade é importante para nós. É política do Construcampi respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site <a href=contru.com.br>CONSTRUCAMPI</a>, e outros sites que possuímos e operamos.</p> 
+						<p>É política do Construcampi respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site <a href=contru.com.br>CONSTRUCAMPI</a>, e outros sites que possuímos e operamos.</p> 
 						<p>Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos coletando e como será usado. </p> 
 						<p>Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando armazenamos dados, protegemos dentro de meios comercialmente aceitáveis ​​para evitar perdas e roubos, bem como acesso, divulgação, cópia, uso ou modificação não autorizados.</p> <p>Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.</p> <p>O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas <a href='https://politicaprivacidade.com' target='_BLANK'>políticas de privacidade</a>. </p> 
-						<p>Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos fornecer alguns dos serviços desejados.</p> <p>O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de privacidade e informações pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do usuário e informações pessoais, entre em contacto connosco.</p> <h2>Política de Cookies Construcampi</h2> <h3>O que são cookies?</h3> <p>Como é prática comum em quase todos os sites profissionais, este site usa cookies, que são pequenos arquivos baixados no seu computador, para melhorar sua experiência. Esta página descreve quais informações eles coletam, como as usamos e por que às vezes precisamos armazenar esses cookies. Também compartilharemos como você pode impedir que esses cookies sejam armazenados, no entanto, isso pode fazer o downgrade ou 'quebrar' certos elementos da funcionalidade do site.</p> 
+						<p>Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos fornecer alguns dos serviços desejados.</p> <p>O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de privacidade e informações pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do usuário e informações pessoais, entre em contacto connosco.</p> 
+						<h2 class="pt-4">POLÍTICA DE COOKIES CONSTRUCAMPI</h2> 
+						<h3>O que são cookies?</h3> 
+						<p>Como é prática comum em quase todos os sites profissionais, este site usa cookies, que são pequenos arquivos baixados no seu computador, para melhorar sua experiência. Esta página descreve quais informações eles coletam, como as usamos e por que às vezes precisamos armazenar esses cookies. Também compartilharemos como você pode impedir que esses cookies sejam armazenados, no entanto, isso pode fazer o downgrade ou 'quebrar' certos elementos da funcionalidade do site.</p> 
 						<h3>Como usamos os cookies?</h3> 
 						<p>Utilizamos cookies por vários motivos, detalhados abaixo. Infelizmente, na maioria dos casos, não existem opções padrão do setor para desativar os cookies sem desativar completamente a funcionalidade e os recursos que eles adicionam a este site. É recomendável que você deixe todos os cookies se não tiver certeza se precisa ou não deles, caso sejam usados ​​para fornecer um serviço que você usa.</p> 
 						<h3>Desativar cookies</h3> 
