7867fb042c96e6027e0009b33b68838c782683be
Gabriel Teixeira <gabriel.404e@gmail.com>
2022-04-15 15:42:21 -0300

    parcerias publicas v.1

--- view/parcerias_publicas.php
+++ view/parcerias_publicas.php
@@ -1,101 +1,85 @@
 <!DOCTYPE HTML>
-<html lang="pt-br">
-
-<head>
-	<title>Quem somos</title>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-	<link rel="stylesheet" href="../public/assets/css/parcerias.css" />
-	<?php include "../includes/css_imports.php"; ?>
-	<meta name="keywords" content="contrucampi,Construcampi,tragetória,Construcampi,arquitetura">
-	<meta property="og:description" content="Venha conhecer mais sobre quem somos, familia Construcampi">
-</head>
-
-<body class="landing is-preload">
-	<!-- Wrapper -->
-	<div id="wrapper">
+<html>
+	<head>
+		<title>Galpoes e Estruturas Metalicas</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="../public/assets/css/parcerias.css" />
+        <?php include "../includes/css_imports.php";?>
+		<meta name="keywords" content="arquitetura,galpões, estruturas, metálicas, estrutura metalica">
+		<meta property="og:description" content="Galpões e estruturas metálicas, de forma mais segura e de fácil acesso">
+	</head>
+	<body class="is-preload">
+		<!-- Header -->
 		<?php include "../includes/header.php"; ?>
+		<!-- Imagem atras -->
 		<!-- Main -->
-		<div id="main_parcerias">
-			<div class="inner">
-				<!-- Banner -->
-				<section id="banner_sobre" class="text-center">
-					<div class="content">
-						<header>
-							<h1 class="h2">Parcerias Publicas e Privadas</h1>
-						</header>
-						<p>Possuímos o mais alto desempenho em inovação e criatividade no mercado, trazendo serviços de qualidade e de alta modernidade no ra</p>
-					</div>
-				</section>
-				<!-- Section -->
-				<section>
-					<div class="features">
-						<article>
-							<span class="image object">
-								<img src="../public/images/cc3.jpg" alt="" class="grow_image" />
-							</span>
-						</article>
-						<article>
-							<span class="icon solid fa-balance-scale"></span>
-							<div class="content">
-								<h3>EFICIÊNCIA</h3>
-								<p>A Construcampi possui como objetivo trazer sempre trabalhos eficientes e de altíssima qualidade para nossos clientes, por isso, contamos com uma equipe sempre disponível e disposta a auxiliar nossos clientes das mais diversas maneiras, seja com consultorias e/ou atendimentos personalizados, nos adequando sempre para corresponder da melhor forma as expectativas de nossos clientes.</p>
-							</div>
-						</article>
-						<article>
-							<span class="icon solid fa-handshake"></span>
-							<div class="content">
-								<h3>QUALIDADE</h3>
-								<p>Na Construcampi, temos como prioridade a entrega de serviços com a maior qualidade dentro do ramo da engenharia civil, sempre com o intuito de trazer satisfação e confiabilidade para atender as suas expectativas.</p>
-							</div>
-						</article>
-					</div>
-				</section>
-
-				<!-- Section -->
-				<section>
-					<header class="major">
-						<h2>Nossa equipe</h2>
+			<section id="second" class="main pb-5">
+				<div class="text_header px-5">
+					<header class="text_border">
+						<h2>Parcerias Públicas e Privadas</h2>
+						<p>Nossas parcerias que apoião a Construcampi transformar sonhos em realidade</p>
 					</header>
-					<div class="posts">
-						<article>
-							<a href="#" class="image"><img src="images/pic01.jpg" alt="" /></a>
-							<h3 class="animate-push-left">Interdum aenean</h3>
-							<p class="animate-push-left">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-						</article>
-						<article>
-							<a href="#" class="image"><img src="images/pic02.jpg" alt="" /></a>
-							<h3 class="animate-push-left">Nulla amet dolore</h3>
-							<p class="animate-push-left">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-						</article>
-						<article>
-							<a href="#" class="image"><img src="images/pic03.jpg" alt="" /></a>
-							<h3 class="animate-push-left">Tempus ullamcorper</h3>
-							<p class="animate-push-left">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-						</article>
-						<article>
-							<a href="#" class="image"><img src="images/pic04.jpg" alt="" /></a>
-							<h3 class="animate-push-left">Sed etiam facilis</h3>
-							<p class="animate-push-left">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-						</article>
-						<article>
-							<a href="#" class="image"><img src="images/pic05.jpg" alt="" /></a>
-							<h3 class="animate-push-left">Feugiat lorem aenean</h3>
-							<p class="animate-push-left">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-						</article>
-						<article>
-							<a href="#" class="image"><img src="images/pic06.jpg" alt="" /></a>
-							<h3 class="animate-push-left">Amet varius aliquam</h3>
-							<p class="animate-push-left">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-						</article>
+				</div>
+				<div class="content dark style2">
+					<div class="container-fluid adjust">
+						<div class="row ml-5 ">
+							<div class="col-12 col-12-narrow overflow_right"> <!-- cuidado! a anim direita tem um overflow diferente do esquerdo, por isso necessita de overflow_right aqui -->
+								<!-- Row grande com o texto -->
+								<div class="row adjust_row push-left">
+									<div class="col-12 col-12-narrow order-md-1 text_container text-center adjust_text">
+										<h3 class="pb-4"> AMCHAM BRASIL </h3>
+										<div>
+											<p>A Amcham vai muito além de uma típica câmara de comércio. Ela integra empresas brasileiras e internacionais em um ambiente que extrapola fronteiras na geração de conteúdo, disponibilizando também um completo e ativo de produtos e serviços empresariais.  <br> Ela integra empresas brasileiras e internacionais em um ambiente que extrapola fronteiras na geração de conteúdo, disponibilizando também um completo e ativo de produtos e serviços empresariais. </p>
+										</div>
+									</div>
+								</div>
+								<!-- row 2 items com animação da direita a origen -->
+								<div id="row_animate_1" class="row right_row_hidden adjust_row">
+									<div class="col-6 col-6-narrow d-flex justify-content-center">
+										<p><a href="https://www.amcham.com.br/" class="link_page"><i class="icon solid text-warning fa-solid fa-arrow-right"></i> Visite a AMCHAM</a></p>
+									</div>
+									<div class="col-6 col-6-narrow pb-5 d-flex justify-content-center">
+										<span class="scale_image">
+											<img class="img-fluid" style="border-radius:1em;" src="../public/images/parcerias/amcham_logo.jpeg" alt="amcham" />
+										</span>
+									</div>
+								</div>
+								<div class="border_hardcoded mt-5"><!-- borda hardcoded --></div>
+								<!-- Row grande com o texto -->
+								<div class="row adjust_row push-left">
+									<div class="col-12 col-12-narrow order-md-1 text_container text-center adjust_text">
+										<h3 class="pb-4"> ADHEMIR FOGASSA MAQUETES </h3>
+										<div>
+											<p>Fundada em 1973, a Adhemir Fogassa Maquetes foi responsável por mais de 8 mil maquetes, colocando-a entre as maiores empresas deste ramo no mundo. Além de exportar toda a qualidade brasileira através de maquetes e modelos reais para diversas cidades da Europa e Estados Unidos, em 2008 expandiu os negócios e conquistou espaço nos países árabes, atuando na capital Abu Dhabi e em Dubai.</p>
+										</div>
+									</div>
+								</div>
+								<!-- row 2 items com animação da direita a origen -->
+								<div id="row_animate_2" class="row right_row_hidden adjust_row">
+									<div class="col-6 col-6-narrow d-flex justify-content-center">
+										<p><a href="https://maquetes.com.br/" class="link_page"><i class="icon solid text-warning fa-solid fa-arrow-right"></i> Visite a FOGASSA MAQUETES</a></p>
+									</div>
+									<div class="col-6 col-6-narrow pb-5 d-flex justify-content-center">
+										<span class="scale_image">
+											<img class="img-fluid" src="../public/images/parcerias/fogassa_logo.png" alt="fogassa_logo" />
+										</span>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
-				</section>
-			</div>
-		</div>
-		<?php include "../includes/footer.php"; ?>
-	</div>
+				</div>
+			</section>
+			<section>
+				<!-- colocar as cidades em uma nova div-->
+			</section>
+		<!-- Footer -->
+			<?php include "../includes/footer.php"; ?>
 
-	<?php include "../includes/scripts_imports.php"; ?>
-</body>
+		<!-- Scripts -->
+		<?php include "../includes/scripts_imports.php"; ?>
+		<script src="../public/assets/js/obras/galpao.js"></script>
 
+	</body>
 </html>
