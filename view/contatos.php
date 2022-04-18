4163913b5870bef8439ba236e128c38b4313071d
Gabriel Teixeira <gabriel.404e@gmail.com>
2022-04-18 16:12:14 -0300

    Fix mobile for contatos

--- view/contatos.php
+++ view/contatos.php
@@ -17,13 +17,13 @@
 				<div class="background p-md-5">
 					<div class="container-fluid adjust d-flex flex-column pb-md-5">
 						<section class="row">
-							<!-- <div class="py-3 px-5">
+							<div class="py-3 px-5">
 								<header class="d-flex justify-content-center py-3 px-5" >
 									<h1 class="h1">
 										<strong>CONTATOS</strong>
 									</h1>
 								</header>
-							</div> -->
+							</div>
 							<div class="column d-flex wrap_div justify-content-around align-content-cent px-md-5">
 								<div class="row divide_div pt-5">
 									<div class="column pt-2 p-3 p-md-0 adjust_text" style="color:#fff">
@@ -48,21 +48,21 @@
 									</div>
 									<div class="row pt-1">
 										<div class="col text-center ">
-											<a href="./contatos.php" class="link_page"><i class="link-primary icon brands fa-facebook" style="font-size:2em;"></i></a>
+											<a href="./contatos.php" class="link_page"><i class="icon_brands link-primary icon brands fa-facebook" style="font-size:2em;"></i></a>
 											<p class="icon_title">Nossa página no facebook</p>
 										</div>
 										<div class="col text-center">
-											<a href="./contatos.php" class="link_page"><i class="link-primary icon brands fa-twitter" style="font-size:2em;"></i></a>
+											<a href="./contatos.php" class="link_page"><i class="icon_brands link-primary icon brands fa-twitter" style="font-size:2em;"></i></a>
 											<p class="icon_title">Nosso perfil no twitter</p>
 										</div>
 										<div class="col text-center">
-											<a href="./contatos.php" class="link_page"><i class="icon link-primary instagram_color icon brands fa-instagram" style="font-size:2em;"></i></a>
+											<a href="./contatos.php" class="link_page"><i class="icon_brands link-primary instagram_color icon brands fa-instagram" style="font-size:2em;"></i></a>
 											<p class="icon_title">Nosso perfil no instagram</p>
 										</div>
 									</div>
 								</div>
 								<div class="row divide_div pt-2 pt-md-5  text-center">
-									<h3>
+									<h3 class="adjust_h3">
 										Venha nos visitar
 									</h3>
 									<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3659.103124475027!2d-46.85204328502365!3d-23.492794784716946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf02394b674fc9%3A0x66136606865c6ceb!2sEdificio%20Amazonia!5e0!3m2!1spt-BR!2sbr!4v1649184281592!5m2!1spt-BR!2sbr" width="100%" height="550px" style="border-radius:1em;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
