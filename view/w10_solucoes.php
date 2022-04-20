bafbc4bdbcabd1cfc770f9330ce54df0efd4fce3
Gabriel Teixeira <gabriel.404e@gmail.com>
2022-04-20 19:18:30 -0300

    add skeleton for w10 solucoes

--- view/w10_solucoes.php
+++ view/w10_solucoes.php
@@ -36,11 +36,11 @@
 							<h2>Magna etiam feugiat</h2>
 							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante sed ex pharetra lacinia sit amet vel massa. Donec facilisis laoreet nulla eu bibendum. Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum dolor sit amet.</p>
 							<ul class="actions stacked">
-								<li><a href="#" class="button">Learn More</a></li>
+								<!-- <li><a href="#" class="button">Learn More</a></li> -->
 							</ul>
 						</div>
 						<div class="image">
-							<img src="images/spotlight01.jpg" alt="" />
+							<img src="../public/images/w10/w10_image.png" alt="" />
 						</div>
 					</section>
 				<!-- Three -->
@@ -49,11 +49,11 @@
 							<h2>Tempus adipiscing</h2>
 							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante sed ex pharetra lacinia sit amet vel massa. Donec facilisis laoreet nulla eu bibendum. Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum dolor sit amet.</p>
 							<ul class="actions stacked">
-								<li><a href="#" class="button">Learn More</a></li>
+								<!-- <li><a href="#" class="button">Learn More</a></li> -->
 							</ul>
 						</div>
 						<div class="image">
-							<img src="images/spotlight02.jpg" alt="" />
+							<img src="../public/images/w10/w10_image.png" alt="" />
 						</div>
 					</section>
 				<!-- Four -->
@@ -62,11 +62,11 @@
 							<h2>Pharetra etiam nulla</h2>
 							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante sed ex pharetra lacinia sit amet vel massa. Donec facilisis laoreet nulla eu bibendum. Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum dolor sit amet.</p>
 							<ul class="actions stacked">
-								<li><a href="#" class="button">Learn More</a></li>
+								<!-- <li><a href="#" class="button">Learn More</a></li> -->
 							</ul>
 						</div>
 						<div class="image">
-							<img src="images/spotlight03.jpg" alt="" />
+							<img src="../public/images/w10/w10_image.png" alt="" />
 						</div>
 					</section>
 				<!-- Five -->
@@ -78,8 +78,8 @@
 						<!-- Gallery -->
 							<div class="gallery style2 medium lightbox onscroll-fade-in">
 								<article>
-									<a href="images/gallery/fulls/01.jpg" class="image">
-										<img src="images/gallery/thumbs/01.jpg" alt="" />
+									<a href="../public/images/w10/w10_image.png" class="image">
+										<img src="../public/images/w10/w10_image.png" alt="" />
 									</a>
 									<div class="caption">
 										<h3>Ipsum Dolor</h3>
@@ -90,8 +90,8 @@
 									</div>
 								</article>
 								<article>
-									<a href="images/gallery/fulls/02.jpg" class="image">
-										<img src="images/gallery/thumbs/02.jpg" alt="" />
+									<a href="../public/images/w10/w10_image.png" class="image">
+										<img src="../public/images/w10/w10_image.png" alt="" />
 									</a>
 									<div class="caption">
 										<h3>Feugiat Lorem</h3>
@@ -102,8 +102,8 @@
 									</div>
 								</article>
 								<article>
-									<a href="images/gallery/fulls/03.jpg" class="image">
-										<img src="images/gallery/thumbs/03.jpg" alt="" />
+									<a href="../public/images/w10/w10_image.png" class="image">
+										<img src="../public/images/w10/w10_image.png" alt="" />
 									</a>
 									<div class="caption">
 										<h3>Magna Amet</h3>
@@ -114,8 +114,8 @@
 									</div>
 								</article>
 								<article>
-									<a href="images/gallery/fulls/04.jpg" class="image">
-										<img src="images/gallery/thumbs/04.jpg" alt="" />
+									<a href="../public/images/w10/w10_image.png" class="image">
+										<img src="../public/images/w10/w10_image.png" alt="" />
 									</a>
 									<div class="caption">
 										<h3>Sed Tempus</h3>
@@ -126,8 +126,8 @@
 									</div>
 								</article>
 								<article>
-									<a href="images/gallery/fulls/05.jpg" class="image">
-										<img src="images/gallery/thumbs/05.jpg" alt="" />
+									<a href="../public/images/w10/w10_image.png" class="image">
+										<img src="../public/images/w10/w10_image.png" alt="" />
 									</a>
 									<div class="caption">
 										<h3>Ultrices Magna</h3>
@@ -138,8 +138,8 @@
 									</div>
 								</article>
 								<article>
-									<a href="images/gallery/fulls/06.jpg" class="image">
-										<img src="images/gallery/thumbs/06.jpg" alt="" />
+									<a href="../public/images/w10/w10_image.png" class="image">
+										<img src="../public/images/w10/w10_image.png" alt="" />
 									</a>
 									<div class="caption">
 										<h3>Sed Tempus</h3>
@@ -150,8 +150,8 @@
 									</div>
 								</article>
 								<article>
-									<a href="images/gallery/fulls/07.jpg" class="image">
-										<img src="images/gallery/thumbs/07.jpg" alt="" />
+									<a href="../public/images/w10/w10_image.png" class="image">
+										<img src="../public/images/w10/w10_image.png" alt="" />
 									</a>
 									<div class="caption">
 										<h3>Ipsum Lorem</h3>
@@ -162,8 +162,8 @@
 									</div>
 								</article>
 								<article>
-									<a href="images/gallery/fulls/08.jpg" class="image">
-										<img src="images/gallery/thumbs/08.jpg" alt="" />
+									<a href="../public/images/w10/w10_image.png" class="image">
+										<img src="../public/images/w10/w10_image.png" alt="" />
 									</a>
 									<div class="caption">
 										<h3>Magna Risus</h3>
@@ -174,8 +174,8 @@
 									</div>
 								</article>
 								<article>
-									<a href="images/gallery/fulls/09.jpg" class="image">
-										<img src="images/gallery/thumbs/09.jpg" alt="" />
+									<a href="../public/images/w10/w10_image.png" class="image">
+										<img src="../public/images/w10/w10_image.png" alt="" />
 									</a>
 									<div class="caption">
 										<h3>Tempus Dolor</h3>
@@ -186,8 +186,8 @@
 									</div>
 								</article>
 								<article>
-									<a href="images/gallery/fulls/10.jpg" class="image">
-										<img src="images/gallery/thumbs/10.jpg" alt="" />
+									<a href="../public/images/w10/w10_image.png" class="image">
+										<img src="../public/images/w10/w10_image.png" alt="" />
 									</a>
 									<div class="caption">
 										<h3>Sed Etiam</h3>
@@ -198,8 +198,8 @@
 									</div>
 								</article>
 								<article>
-									<a href="images/gallery/fulls/11.jpg" class="image">
-										<img src="images/gallery/thumbs/11.jpg" alt="" />
+									<a href="../public/images/w10/w10_image.png" class="image">
+										<img src="../public/images/w10/w10_image.png" alt="" />
 									</a>
 									<div class="caption">
 										<h3>Magna Lorem</h3>
@@ -210,8 +210,8 @@
 									</div>
 								</article>
 								<article>
-									<a href="images/gallery/fulls/12.jpg" class="image">
-										<img src="images/gallery/thumbs/12.jpg" alt="" />
+									<a href="../public/images/w10/w10_image.png" class="image">
+										<img src="../public/images/w10/w10_image.png" alt="" />
 									</a>
 									<div class="caption">
 										<h3>Ipsum Dolor</h3>
