88d9b5a7576f8ede412fc275db0ccd0012908c35
Gabriel Teixeira <gabriel.404e@gmail.com>
2022-04-22 16:59:05 -0300

    site definitivo

---
+++ paginas/obras_casas.php
@@ -0,0 +1,111 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>Casas Pre-fabricadas</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="stylesheet" href="../public/assets/css/obras.css" />
+	<?php include "../includes/css_imports.php"; ?>
+	<meta name="keywords" content="arquitetura,casas, pré-fabricadas, engenharia">
+	<meta property="og:description" content="Casas pré-fabricadas, nada como a facilidade de ter sua casa de forma mais rápida">
+</head>
+
+<body class="is-preload">
+	<!-- Header -->
+	<?php include "../includes/header.php"; ?>
+	<!-- Main -->
+	<section id="second" class="main pb-5">
+		<header class="imagem_com_texto">
+			<h2>Casas Pré-Fabricadas e Pré-Moduladas</h2>
+			<p>Engenharia modular de rápida e efetiva implantação sem custos abusivos</p>
+		</header>
+		<div class="content dark style2">
+			<div class="container-fluid adjust">
+				<div class="row ml-5 ">
+					<div class="col-12 col-12-narrow overflow_right">
+						<!-- cuidado! a anim direita tem um overflow diferente do esquerdo, por isso necessita de overflow_right aqui -->
+						<!-- row 3 items com animação da direita a origen sem texto -->
+						<div id="row_animate_1" class="row right_row_hidden">
+							<div class="col-4 text_container col-6-narrow pb-5">
+								<span class="scale_image adjust_img">
+									<img class="grow_image2 img-fluid rounded" src="../public/images/obras/fabricadas/casas-1.jpg" alt="casa_pre_fabricada" />
+								</span>
+							</div>
+							<div class="col-4 text_container col-6-narrow">
+								<span class="scale_image adjust_img">
+									<img class="grow_image2 img-fluid rounded" src="../public/images/obras/fabricadas/casas-3.jpg" alt="casa_pre_fabricada" />
+								</span>
+							</div>
+							<div class="col-4 text_container col-12-narrow pb-5">
+								<span class="scale_image adjust_img">
+									<img class="grow_image2 img-fluid rounded" src="../public/images/obras/fabricadas/casas-2.jpg" alt="casa_pre_fabricada" />
+								</span>
+							</div>
+						</div>
+						<!-- Texto cercado por imagens -->
+						<div class="row adjust_row push-left">
+							<div class="col-4 col-12-narrow text_container pb-5 adjust_image">
+								<span class="scale_image  border_activate">
+									<img class="grow_image2 img-fluid rounded mx-auto d-block" src="../public/images/obras/fabricadas/casas-4.jpg" alt="casa_pre_fabricada" />
+								</span>
+								<div class="hidden_text">
+									Casa Pré-Fabricada (Exterior)
+								</div>
+							</div>
+							<div class="col-4 col-12-narrow text_container text-center adjust_text">
+								<h3 class="pb-4"> Modularidade</h3>
+								<div>
+									<p>Arquiteturas onde o cliente apenas paga pelo que <strong>será utilizado</strong>, este é o propósito das casas <strong>Pré-Fabricadas</strong> e <strong>Pré-Moduladas da Construcampi</strong>.<br> </p>
+									<p>Utilizando das práticas de engenharia moderna em pricipal a <strong>construção off-site</strong> (construção remota), trazemos <strong>luxo</strong> não apenas na <strong>área exterior</strong> mas também na <strong>área interior</strong> com pré-criação; esta a qual pode ser <strong>facilmente redesenhada</strong> para a expecificação de nossos clientes.</p>
+									<p><a href="./contatos.php" class="link_page"><i class="icon solid text-danger fa-solid fa-arrow-right"></i> Entre em contato conosco</a></p>
+								</div>
+							</div>
+							<div class="col-4 col-12-narrow text_container  pb-5 adjust_image">
+								<span class="scale_image  border_activate">
+									<img class="grow_image2 img-fluid rounded mx-auto d-block" src="../public/images/obras/fabricadas/casas-5.jpg" alt="casa_pre_fabricada" />
+								</span>
+								<div class="hidden_text">
+									Casa Pré-Fabricada (Interior)
+								</div>
+							</div>
+						</div>
+						<!-- row 3 items com animação da esquerda a origen sem texto -->
+						<div id="row_animate_2" class="row left_row_hidden">
+							<div class="col-4 text_container col-6-narrow pb-5">
+								<span class="scale_image adjust_img">
+									<img class="grow_image2 img-fluid rounded" src="../public/images/obras/fabricadas/casas-6.jpg" alt="casa_pre_fabricada" />
+								</span>
+
+							</div>
+							<div class="col-4 text_container col-6-narrow">
+								<span class="scale_image adjust_img">
+									<img class="grow_image2 img-fluid rounded" src="../public/images/obras/fabricadas/casas-7.jpg" alt="casa_pre_fabricada" />
+								</span>
+
+							</div>
+							<div class="col-4 text_container col-12-narrow pb-5">
+								<span class="scale_image adjust_img">
+									<img class="grow_image2 img-fluid rounded" src="../public/images/obras/fabricadas/casas-8.jpg" alt="casa_pre_fabricada" />
+								</span>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section>
+		<!-- colocar as cidades em uma nova div-->
+	</section>
+	<!-- Footer -->
+	<?php include "../includes/footer.php"; ?>
+
+	<!-- Scripts -->
+	<?php include "../includes/scripts_imports.php"; ?>
+	<script src="../public/assets/js/obras/casas.js"></script>
+
+</body>
+
+</html>
