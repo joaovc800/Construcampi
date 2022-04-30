980bd4f7d3e435b28b974888e3e973713fc7ff16
Gabriel Teixeira <gabriel.404e@gmail.com>
2022-04-30 18:08:34 -0300

    fix font

--- deployFiles/public/assets/js/home/homeAnim.js
+++ deployFiles/public/assets/js/home/homeAnim.js
@@ -53,8 +53,8 @@ let anim = `
 	<div class="p-5 p-md-0 col-12 col-md-4">
 		<img class="img-fluid image-spin" alt="Construcampi Logo" src="public/images/logoAnim/logo_anim_no_bg_lowest.png"></img>
 	</div>
-	<h1 class="heading h1 col-12 col-md-4 text-justify text-danger">Bem Vindo a Construcampi</h1>
-	<h3 class="sub-heading col-12 col-md-4 text-light">Seu objetivo é o Nosso Principal Compromisso e Responsabilidade</h3>
+	<h1 class="heading_anim h1 col-12 col-md-4 text-justify text-danger">Bem Vindo a Construcampi</h1>
+	<h3 class="sub-heading_anim col-12 col-md-4 text-light">Seu objetivo é o Nosso Principal Compromisso e Responsabilidade</h3>
 	<div class="loader-wrapper">
 	<div class="loader"></div>
 	</div>
@@ -71,6 +71,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	.add({
 		opacity: ["0","1"],
 		duration: 1400
+	
 	})
 	.add({
 		delay: 2700,
