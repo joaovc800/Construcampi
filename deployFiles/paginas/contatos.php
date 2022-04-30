1b67d1cbe1b9a21f9c6674bceff0be9956ad97a3
Gabriel Teixeira <gabriel.404e@gmail.com>
2022-04-29 23:10:00 -0300

    adicionar contatos

--- deployFiles/paginas/contatos.php
+++ deployFiles/paginas/contatos.php
@@ -20,8 +20,8 @@
 				<div class="background p-md-5">
 					<div class="container-fluid adjust d-flex flex-column pb-md-5">
 						<section class="row">
-							<div class="py-3 px-5">
-								<header class="d-flex justify-content-center py-3 px-5" >
+							<div class="py-3 px-md-5">
+								<header class="d-flex justify-content-center py-3 px-0 px-md-5" >
 									<h1 class="h1">
 										<strong>Entre em Contato Conosco</strong>
 									</h1>
@@ -29,27 +29,64 @@
 							</div>
 							<div class="column d-flex wrap_div justify-content-around align-content-cent px-md-5">
 								<div class="row divide_div pt-5">
-									<div class="column pt-2 p-3 p-md-0 adjust_text" style="color:#fff">
+										<div class="column pt-2 p-3 p-md-0 adjust_text" style="color:#fff">
+											<h3 class="text-center default_color">
+												Informações de Contato
+											</h3>
 										<div class="row">
 											<div class="col-12">
 												<div class="text-center p-3 pt-4" style="background-color:#222; border-radius:1em;"><i class="icon solid text-danger fa fa-map-marker" style="font-size:2em;" aria-hidden="true"></i> <p>Endereço: Ed. Amazonia - Alameda Mamoré, Alphaville - SP</p></div> 
 											</div>
 										</div>
 										<div class="row pt-5">
-											<div class="col-6">
+											<div class="col-12">
 												<div class="d-flex flex-column text-center p-3 pt-4" style="background-color:#222; border-radius:1em; height: 100%;"><i class="icon solid text-danger fa fa-phone" style="font-size:2em;"></i><p>	Telefone: +55 11 99199-8401</p></div> 
 											</div>
-											<div class="col-6">
-												<div class="text-center p-3 pt-4" style="background-color:#222; border-radius:1em;"><i class="icon text-danger fa fa-envelope" style="font-size:2em;"></i><p style="word-wrap: break-word;"> Email: construcampiconstrucoesltda@hotmail.com</p></div> 
+										</div>
+										<div class="row pt-5">
+											<div class="col-12">
+												<div class="text-center p-3 pt-4" style="background-color:#222; border-radius:1em;">
+													<i class="icon text-danger fa fa-envelope" style="font-size:2em;"></i>
+													<p>Emails de Contato</p>
+													<div class="column pt-0 pt-md-3">
+														<div class="row adjust_text_p">
+															<p class="col-12 col-md-4">comercial@construcampi.com.br</p>
+															<p class="col-12 col-md-4">ti@construcampi.com.br</p>
+															<p class="col-12 col-md-4">w10solucoessustentaveis@construcampi.com.br</p>
+														</div>
+														<div class="row adjust_text_p">
+															<p class="col-12 col-md-4">financeiro@construcampi.com.br</p>
+															<p class="col-12 col-md-4">parcerias@construcampi.com.br</p>
+															<p class="col-12 col-md-4">solicitacaodeorcamentodeobras@construcampi.com.br</p>
+														</div>
+														<div class="row adjust_text_p">
+															<p class="col-12 col-md-4">juridico@construcampi.com.br</p>
+															<p class="col-12 col-md-4">relacoesinternacionais@construcampi.com.br</p>
+															<p class="col-12 col-md-4">relacoescominvestidores@construcampi.com.br</p>
+														</div>
+														<div class="row adjust_text_p">
+															<p class="col-12 col-md-4">dep.engenharia@construcampi.com.br</p>
+															<p class="col-12 col-md-4">dep.projetosprivados@construcampi.com.br</p>
+															<p class="col-12 col-md-4">dep.projetospublicos@construcampi.com.br</p>
+														</div>
+													</div>
+												</div> 
 											</div>
 										</div>
 									</div>
-									<div class="col col-12-narrow d-flex justify-content-center py-3 py-md-0 pt-md-5">
+									
+								</div>
+								<div class="row divide_div pt-2 pt-md-5  text-center">
+									<h3 class="order-3 order-md-1 adjust_h3">
+										Venha nos visitar
+									</h3>
+									<iframe class="order-4 order-md-2 iframe_adjust" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3659.103124475027!2d-46.85204328502365!3d-23.492794784716946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf02394b674fc9%3A0x66136606865c6ceb!2sEdificio%20Amazonia!5e0!3m2!1spt-BR!2sbr!4v1649184281592!5m2!1spt-BR!2sbr" width="100%" height="550px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+									<div class="order-1 order-md-3 col col-12-narrow d-flex justify-content-center py-3 py-md-0 pt-md-5 ">
 										<h4>
 											Entre também em contato conosco pelas nossas redes sociais
 										</h4>
 									</div>
-									<div class="row pt-1">
+									<div class="order-2 order-md-4 row pt-1">
 										<div class="col text-center ">
 											<a href="./contatos" class="link_page"><i class="icon_brands link-primary icon brands fa-facebook" style="font-size:2em;"></i></a>
 											<p class="icon_title">Nossa página no facebook</p>
@@ -64,12 +101,7 @@
 										</div>
 									</div>
 								</div>
-								<div class="row divide_div pt-2 pt-md-5  text-center">
-									<h3 class="adjust_h3">
-										Venha nos visitar
-									</h3>
-									<iframe class="iframe_adjust" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3659.103124475027!2d-46.85204328502365!3d-23.492794784716946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf02394b674fc9%3A0x66136606865c6ceb!2sEdificio%20Amazonia!5e0!3m2!1spt-BR!2sbr!4v1649184281592!5m2!1spt-BR!2sbr" width="100%" height="550px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-								</div>
+								
 							</div>
 						</section>
 					</div>
